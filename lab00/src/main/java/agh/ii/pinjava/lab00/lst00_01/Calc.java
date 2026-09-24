69379f8687b4a0269588e8888fdfe8ed1c8e1b2d
joycefopa <cerenajoyce55@gmail.com>
2026-09-24 02:56:48 +0200

    nouveauDoc

---
+++ lab00/src/main/java/agh/ii/pinjava/lab00/lst00_01/Calc.java
@@ -0,0 +1,7 @@
+package agh.ii.pinjava.lab00.lst00_01;
+
+public class Calc {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ lab00/src/test/java/agh/ii/pinjava/lab00/lst00_01/CalcTest.java
@@ -0,0 +1,26 @@
+package agh.ii.pinjava.lab00.lst00_01;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+    @BeforeEach
+    void setUp() {
+        System.out.println("CalcTest.setUp...");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("CalcTest.tearDown...");
+    }
+
+    @Test
+    void oneAddTwoIsThree() {
+        // If
+        int a = 1, b = 2;
+        // then
+        assertEquals(3, Calc.add(a,b));
+    }
+}
